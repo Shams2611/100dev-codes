9b5fad4357cd29c313ea3b2cab5b6b9003d925fd
Shams Abrar <shams.abrar2611@gmail.com>
2024-03-21 05:14:00 -0400

    Bachelor code

--- class08/temperature-converter/js/main.js
+++ class08/temperature-converter/js/main.js
@@ -1,1 +1,15 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+
+// Take input from user in celcius
+// run a conditional statement as if the input is valid
+// have a function that converts celcius to fahrenheit
+// return the value of function and console log it
+
+document.querySelector("h1").addEventListener("click", convert);
+
+function convert() {
+  let enteredVal = document.querySelector("input").value;
+  let temp = enteredVal * 1.8 + 32;
+
+  document.querySelector("h2").innerText = temp;
+}
