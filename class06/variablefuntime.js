2faf6a95d942f2c45452093e767ef64f13c48317
Shams Abrar <shams.abrar2611@gmail.com>
2024-03-21 03:18:46 -0400

    Some js practice after a while

---
+++ class06/variablefuntime.js
@@ -0,0 +1,23 @@
+// PHILADELPHIA (CBS) -- The Philadelphia Department of Public Health officially announced the end of the citywide measles outbreak that spiked between December 2023 and January 2024 on Tuesday.
+
+//The outbreak impacted nine people, eight of whom -- two adults and six children -- were from Philadelphia. Health Department officials said six of the positive Philadelphia cases were hospitalized at one point but have since been released.
+
+let num = 5;
+
+num -= 10;
+
+console.log(num);
+
+let value = document.querySelector("input").value;
+
+value += 25;
+
+alert(value);
+
+const h1 = document.querySelector("h1");
+
+h1.addEventListener("click", addTwoNums);
+
+function addTwoNums() {
+  console.log(num + value);
+}
